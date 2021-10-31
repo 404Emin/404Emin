2d2f999e79a0d904523dd2c3e49d9a70803fa672
404Emin <alczylok@gmail.com>
2021-10-31 20:03:41 +0100

    human

--- TextGame.py
+++ TextGame.py
@@ -1,16 +1,17 @@
 import time
 import os
-from typing_extensions import runtime
 
 #Defs
 def clear():
     os.system (" cls ")
 
+#HP
 zombieHP = (105)
 trollHP = (130)
 skeletonHP = (95)
+humanHP = (70)
 
-
+# HP processing
 zombie = (input("Ammount of zombies... "))
 
 if zombie.isnumeric():
@@ -35,6 +36,15 @@ else:
     print ("Thats not a number idiot...")
     exit()
 
+human = input("Ammount of humans... ")
+
+if human.isnumeric():
+    human2 = int(human) * humanHP
+else:
+    print ("Thats not a number idiot...")
+    exit()
+
+#fight process..
 if int(zombie2) > int(troll2):
     W1 = int(zombie2)
     w1= ("Zombie wins!")
@@ -44,7 +54,6 @@ elif int(zombie2) < int(troll2):
 else:
     W1 = int(zombie2)
     w1= ("Zombie wins!")
-
 
 if int(W1) > int(skeleton2):
     W2 = int(W1)
@@ -56,5 +65,15 @@ else:
     W2 = int(skeleton2)
     w2 = ("Skeleton wins!")
 
+if int(W2) > int(human2):
+    W3 = int(W2)
+    w3 = (w2)
+elif int(W2) < int(human2):
+    W3 = int(W2)
+    w3 = ("Human wins!")
+else:
+    W3 = int(W2)
+    w3 = ("Skeleton wins!")
+
 clear
-print (w2)
+print (w3)
